cab4b716e69577d3850a71d28313ec441630c6b5
faemous <luckyabiom@yahoo.com>
2022-03-11 15:24:26 -0800

    finally

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -30,7 +30,7 @@ putchar(g);
 putchar(' ');
 putchar(e);
 putchar(i);
-if (h == 57 && g == 56 && e== 57 && i == 57)
+if (h == 57 && g == 56 && e == 57 && i == 57)
 break;
 putchar(',');
 putchar(' ');
